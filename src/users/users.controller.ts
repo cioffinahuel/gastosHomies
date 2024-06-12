8f9dfa042d7ca8e2e52d5e46ad816bd8fc9251c5
Nahuel Cioffi <ntcioffi@gmail.com>
2024-06-11 21:22:24 -0300

    Bearer auth

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,15 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller()
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
-  
   @Get('/users')
+  @ApiTags('users')
   getUsers() {
     return this.usersService.getUsers();
   }
@@ -17,5 +18,12 @@ export class UsersController {
   @ApiTags('users')
   createUser(@Body() user: CreateUserDto) {
     return this.usersService.createUser(user);
+  }
+
+  @UseGuards(AuthGuard)
+  @ApiTags('users')
+  @Get('/users/:email') 
+  getUserByEmail(@Param('email') email: string){ 
+    return this.usersService.findOne(email); 
   }
 }
